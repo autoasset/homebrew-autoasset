470b8b2ce7f5093d7c8a0230350329f7cba56317
linhey <158179948@qq.com>
2020-06-05 17:17:42 +0800

    Update autoasset.rb

--- Formula/autoasset.rb
+++ Formula/autoasset.rb
@@ -4,7 +4,7 @@
 class Autoasset < Formula
   desc "iOS 资源文件管理脚本"
   homepage "https://github.com/autoasset/autoasset"
-  url "https://raw.githubusercontent.com/autoasset/homebrew-autoasset/master/Autoasset-9.tar.gz"
+  url "https://github.com/autoasset/autoasset/releases/download/9/autoasset-9.tar.gz"
   sha256 "1f208464788c84a0d0b3e706c3d494c05e4535a3dfa7b519248b07038b58b6e2"
 
   def install
