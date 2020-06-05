8c56f3ce8c84491d5b85ab31177007bb36c7e50c
linhey <158179948@qq.com>
2020-06-05 16:54:52 +0800

    Update autoasset.rb

--- Formula/autoasset.rb
+++ Formula/autoasset.rb
@@ -7,28 +7,11 @@ class Autoasset < Formula
   url "https://raw.githubusercontent.com/autoasset/homebrew-autoasset/master/Autoasset-9.tar.gz"
   sha256 "1f208464788c84a0d0b3e706c3d494c05e4535a3dfa7b519248b07038b58b6e2"
 
-  # depends_on "cmake" => :build
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
+    bin.install "Autoasset"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test Autoasset`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
