476f56f6f0ff4aa62529ac1186f7181c2e1b6333
林翰 <linhan@dxy.cn>
2020-06-05 16:24:16 +0800

    update

--- Formula/autoasset.rb
+++ Formula/autoasset.rb
@@ -2,8 +2,8 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Autoasset < Formula
-  desc ""
-  homepage ""
+  desc "iOS 资源文件管理脚本"
+  homepage "https://github.com/autoasset/autoasset"
   url "https://raw.githubusercontent.com/autoasset/homebrew-autoasset/master/Autoasset-9.tar.gz"
   sha256 "1f208464788c84a0d0b3e706c3d494c05e4535a3dfa7b519248b07038b58b6e2"
 
