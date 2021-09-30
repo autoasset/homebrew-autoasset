0390ec0973aff36f845e1e3385c547c1b14888ba
linhey <158179948@qq.com>
2021-09-30 14:55:09 +0800

    Update autoasset.rb

--- Formula/autoasset.rb
+++ Formula/autoasset.rb
@@ -4,8 +4,8 @@
 class Autoasset < Formula
   desc "iOS 资源文件管理脚本"
   homepage "https://github.com/autoasset/autoasset"
-  url "https://github.com/autoasset/autoasset/releases/download/40/autoasset-40.tar.gz"
-  sha256 "d866012d6289d5b24e60f81f4dd71fab36189568735d59cc2bd1f945e4e48340"
+  url "https://github.com/autoasset/autoasset/releases/download/41/autoasset-41.tar.gz"
+  sha256 "5593238a162ce3f2de1cd7d87fd0ebcf802ff492a48ab0d97519603ba946010a"
 
   def install
     bin.install "autoasset"
